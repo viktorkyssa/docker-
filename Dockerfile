762d253563ff1b22a904e90adacdd4ba1a6539eb
viktor <vkibank@gmail.com>
2020-03-10 08:43:32 +0200

    Web-server image and throw port

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,6 @@ WORKDIR /usr/src/app/
 
 COPY . /usr/src/app/
 
+EXPOSE 5000
+
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-setInterval(function() {
-	console.log('Hello World!!!');
-}, 1000);
+// setInterval(function() {
+// 	console.log('Hello World!!!');
+// }, 1000);
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+	res.status(201).json({
+		name: "Viktor"
+	});
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
