d3c98db33ddcd69cb469503b7cbc7484ec33f188
viktor <vkibank@gmail.com>
2020-03-11 08:37:02 +0200

    Volume

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-const resources = require("./resources/response.json");
+const resources = require("./res/response.json");
 
 app.get('/', (req, res) => {
 	res.status(201).json({
