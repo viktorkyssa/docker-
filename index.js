52416b28ba27ee5ac8c1cff2be1281b49c71bd96
viktor <vkibank@gmail.com>
2020-03-12 09:23:26 +0200

    MongoDB, write users in collection

--- index.js
+++ index.js
@@ -4,13 +4,36 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-const resources = require("./res/response.json");
+// const resources = require("./res/response.json");
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/';
+const mongoClient = MongoClient(url, {useNewUrlParser: true});
+
+
 
 app.get('/', (req, res) => {
-	res.status(201).json({
-		name: resources.name,
-		time: new Date()
-	});
+	// res.status(201).json({
+	// 	name: resources.name,
+	// 	time: new Date()
+	// });
 });
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => {
+	console.log(`Server started on port ${port}`);
+	mongoClient.connect(function(err, client) {
+		const db = client.db('usersdb');
+		const collection = db.collection('users');
+		let user = {name: 'Tom2', age: 23};
+		collection.insertOne(user, function(error, result) {
+			if(err) {
+				return console.log(err);
+			}
+
+			console.log(result.ops);
+		});
+		collection.find().toArray().then(function(items) {
+			console.log(items);
+		});
+		client.close();
+	});
+});
